00cb7a70bf7d3302e9b08f6fa4c141448feb215f
jonathan.philip@gmail.com <jonathan.philip@gmail.com>
2018-01-21 17:19:59 +1100

    Starting frontend for race details

---
+++ RaceDay/RaceDay/ClientApp/app/components/races/races.component.ts
@@ -0,0 +1,43 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'races',
+    templateUrl: './races.component.html'
+})
+export class RacesComponent {
+    public races: RaceDetails[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get('http://localhost:7344/api/race').subscribe(result => {
+            this.races = result.json() as RaceDetails[];
+        }, error => console.error(error));
+    }
+}
+
+interface RaceDetails {
+    race: Race;
+    status: string;
+    totalPlace: number;
+    Horses: HorseDetails[];
+}
+
+interface Race {
+    id: number;
+    name: string;
+    start: Date;
+    status: string;
+    Horses: Horse[]
+}
+
+interface Horse {
+    id: number;
+    name: string;
+    odds: string;
+}
+
+interface HorseDetails {
+    horse: Horse;
+    numberOfBets: number;
+    totalPotentialPayout: number;
+}
